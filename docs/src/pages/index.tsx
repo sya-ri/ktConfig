0af84ff092025c8172b37bbe91605ad1611d902e
sya-ri <git@s7a.dev>
2022-11-09 14:23:27 +0900

    docs: Tweak homepage

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Get started with ktConfig
+            Get started
           </Link>
         </div>
       </div>
