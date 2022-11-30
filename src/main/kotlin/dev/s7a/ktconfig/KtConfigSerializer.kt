870f6c2a4e1cc8024a2b9e72cb3bd031ceda9b7c
sya-ri <git@s7a.dev>
2022-11-30 10:44:11 +0900

    docs: KtConfigSerializer kdoc

--- src/main/kotlin/dev/s7a/ktconfig/KtConfigSerializer.kt
+++ src/main/kotlin/dev/s7a/ktconfig/KtConfigSerializer.kt
@@ -2,10 +2,34 @@ package dev.s7a.ktconfig
 
 import kotlin.reflect.KType
 
+/**
+ * User-defined type serializer.
+ *
+ * @since 1.0.0
+ */
 interface KtConfigSerializer {
+    /**
+     * Config-storable type.
+     *
+     * @since 1.0.0
+     */
     val type: KType
 
+    /**
+     * Convert config-storable type to user-defined type.
+     *
+     * @param value Type is [type].
+     * @return User-defined type.
+     * @since 1.0.0
+     */
     fun deserialize(value: Any?): Any?
 
+    /**
+     * Convert user-defined type to config-storable type.
+     *
+     * @param value Type is [deserialize] return type.
+     * @return Type is [type].
+     * @since 1.0.0
+     */
     fun serialize(value: Any?): Any?
 }
--- src/main/kotlin/dev/s7a/ktconfig/UseSerializer.kt
+++ src/main/kotlin/dev/s7a/ktconfig/UseSerializer.kt
@@ -2,5 +2,11 @@ package dev.s7a.ktconfig
 
 import kotlin.reflect.KClass
 
+/**
+ * Use a user-defined type serializer.
+ *
+ * @property with Class that extends [KtConfigSerializer]
+ * @since 1.0.0
+ */
 @Target(AnnotationTarget.TYPE)
 annotation class UseSerializer(val with: KClass<out KtConfigSerializer>)
