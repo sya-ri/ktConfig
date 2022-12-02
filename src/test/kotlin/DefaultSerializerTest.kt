ecf790953a5d6412b6c848b02782c3313f7482af
sya-ri <git@s7a.dev>
2022-12-02 10:04:00 +0900

    feat(api): Add LocationString

---
+++ src/test/kotlin/DefaultSerializerTest.kt
@@ -0,0 +1,29 @@
+import be.seeseemelk.mockbukkit.MockBukkit
+import dev.s7a.ktconfig.ktConfigString
+import dev.s7a.ktconfig.saveKtConfigString
+import dev.s7a.ktconfig.serializer.LocationString
+import org.bukkit.Location
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class DefaultSerializerTest {
+    @Test
+    fun location() {
+        data class Data(val data: LocationString?)
+
+        val server = MockBukkit.mock()
+        val world = server.addSimpleWorld("test")
+        assertEquals("data: null, 5.0, 2.0, -1.5\n", saveKtConfigString(Data(Location(null, 5.0,  2.0, -1.5))))
+        assertEquals("data: test, 5.0, 2.0, -1.5\n", saveKtConfigString(Data(Location(world, 5.0,  2.0, -1.5))))
+        assertEquals("data: test, 5.0, 2.0, -1.5, 90.0, 31.5\n", saveKtConfigString(Data(Location(world, 5.0,  2.0, -1.5, 90F, 31.5F))))
+        assertEquals(Data(null), ktConfigString("data: null"))
+        assertEquals(Data(null), ktConfigString("data: null, 5"))
+        assertEquals(Data(null), ktConfigString("data: null, 5, 2"))
+        assertEquals(Data(Location(null, 5.0, 2.0, -1.5)), ktConfigString("data: null, 5, 2, -1.5"))
+        assertEquals(Data(Location(null, 5.0, 2.0, -1.5, 30F, 0F)), ktConfigString("data: null, 5, 2, -1.5, 30"))
+        assertEquals(Data(Location(world, 5.0, 2.0, -1.5)), ktConfigString("data: test, 5, 2, -1.5"))
+        assertEquals(Data(Location(world, 5.0, 2.0, -1.5)), ktConfigString("data: test, 5, 2, -1.5, 0, 0"))
+        assertEquals(Data(Location(world, 5.0, 2.0, -1.5, 90F, 31.5F)), ktConfigString("data: test, 5, 2, -1.5, 90, 31.5"))
+        MockBukkit.unmock()
+    }
+}
