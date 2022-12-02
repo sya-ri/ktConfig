c911d263af9beafd57d006e6abbde03248e44472
sya-ri <git@s7a.dev>
2022-12-02 10:57:40 +0900

    feat(api): Add BlockString

---
+++ src/main/kotlin/dev/s7a/ktconfig/serializer/BlockString.kt
@@ -0,0 +1,43 @@
+package dev.s7a.ktconfig.serializer
+
+import dev.s7a.ktconfig.KtConfigSerializer
+import dev.s7a.ktconfig.UseSerializer
+import org.bukkit.Bukkit
+import org.bukkit.block.Block
+import kotlin.reflect.typeOf
+
+/**
+ * [Block] separated by commas.
+ *
+ * @since 1.0.0
+ * @see Block
+ */
+typealias BlockString = @UseSerializer(BlockStringSerializer::class) Block
+
+/**
+ * Serializer of [Block] separated by commas.
+ *
+ * @since 1.0.0
+ */
+object BlockStringSerializer : KtConfigSerializer {
+    override val type = typeOf<String>()
+
+    override fun deserialize(value: Any?): Block? {
+        require(value is String)
+        return runCatching {
+            value.split(',').let {
+                if (it.size != 4) return null
+                val world = Bukkit.getWorld(it[0].trim()) ?: return null
+                val x = it[1].trim().toInt()
+                val y = it[2].trim().toInt()
+                val z = it[3].trim().toInt()
+                world.getBlockAt(x, y, z)
+            }
+        }.getOrNull()
+    }
+
+    override fun serialize(value: Any?): String {
+        require(value is Block)
+        return "${value.world.name}, ${value.x}, ${value.y}, ${value.z}"
+    }
+}
--- src/test/kotlin/DefaultSerializerTest.kt
+++ src/test/kotlin/DefaultSerializerTest.kt
@@ -1,19 +1,50 @@
 import be.seeseemelk.mockbukkit.MockBukkit
+import be.seeseemelk.mockbukkit.ServerMock
 import dev.s7a.ktconfig.ktConfigString
 import dev.s7a.ktconfig.saveKtConfigString
+import dev.s7a.ktconfig.serializer.BlockString
 import dev.s7a.ktconfig.serializer.LocationString
 import dev.s7a.ktconfig.serializer.VectorString
 import org.bukkit.Location
 import org.bukkit.util.Vector
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class DefaultSerializerTest {
+    private lateinit var server: ServerMock
+
+    @BeforeTest
+    fun setup() {
+        server = MockBukkit.mock()
+    }
+
+    @AfterTest
+    fun teardown() {
+        MockBukkit.unmock()
+    }
+
+    @Test
+    fun block() {
+        data class Data(val data: BlockString?)
+
+        val world = server.addSimpleWorld("test")
+        assertEquals("data: test, -1, 3, 5\n", saveKtConfigString(Data(world.getBlockAt(-1, 3, 5))))
+        assertEquals(Data(null), ktConfigString("data: null"))
+        assertEquals(Data(null), ktConfigString("data: null, -1"))
+        assertEquals(Data(null), ktConfigString("data: null, -1, 3"))
+        assertEquals(Data(null), ktConfigString("data: null, -1, 3, 5"))
+        assertEquals(Data(null), ktConfigString("data: test"))
+        assertEquals(Data(null), ktConfigString("data: test, -1"))
+        assertEquals(Data(null), ktConfigString("data: test, -1, 3"))
+        assertEquals(Data(world.getBlockAt(-1, 3, 5)), ktConfigString("data: test, -1, 3, 5"))
+    }
+
     @Test
     fun location() {
         data class Data(val data: LocationString?)
 
-        val server = MockBukkit.mock()
         val world = server.addSimpleWorld("test")
         assertEquals("data: null, 5.0, 2.0, -1.5\n", saveKtConfigString(Data(Location(null, 5.0,  2.0, -1.5))))
         assertEquals("data: test, 5.0, 2.0, -1.5\n", saveKtConfigString(Data(Location(world, 5.0,  2.0, -1.5))))
@@ -26,7 +57,6 @@ class DefaultSerializerTest {
         assertEquals(Data(Location(world, 5.0, 2.0, -1.5)), ktConfigString("data: test, 5, 2, -1.5"))
         assertEquals(Data(Location(world, 5.0, 2.0, -1.5)), ktConfigString("data: test, 5, 2, -1.5, 0, 0"))
         assertEquals(Data(Location(world, 5.0, 2.0, -1.5, 90F, 31.5F)), ktConfigString("data: test, 5, 2, -1.5, 90, 31.5"))
-        MockBukkit.unmock()
     }
 
     @Test
