868dd6ee2fd543b932fd6d45bef227a0d7aaf669
github-action-benchmark <github@users.noreply.github.com>
2024-09-16 14:58:41 +0000

    add JMH Benchmark (jmh) benchmark result for 100a3fd0352708c65e783ceb8a9d38f94780a726

---
+++ data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1726498721469,
+  "repoUrl": "https://github.com/sya-ri/ktConfig",
+  "entries": {
+    "JMH Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "git@s7a.dev",
+            "name": "sya-ri",
+            "username": "sya-ri"
+          },
+          "committer": {
+            "email": "git@s7a.dev",
+            "name": "sya-ri",
+            "username": "sya-ri"
+          },
+          "distinct": true,
+          "id": "100a3fd0352708c65e783ceb8a9d38f94780a726",
+          "message": "ci: Fix benchmark branch",
+          "timestamp": "2024-09-16T23:49:05+09:00",
+          "tree_id": "c4d759a4ba9a7d3ae75d88d4fd55843f3667f326",
+          "url": "https://github.com/sya-ri/ktConfig/commit/100a3fd0352708c65e783ceb8a9d38f94780a726"
+        },
+        "date": 1726498720637,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "dev.s7a.ktconfig.Benchmark.single",
+            "value": 0.007161375338282236,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
