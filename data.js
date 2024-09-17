0171b1a948815a30d9ebb9d6d3de7c2e284728b1
sya-ri <git@s7a.dev>
2024-09-18 02:37:39 +0900

    Sort benchmark tests

--- data.js
+++ data.js
@@ -16,96 +16,6 @@ window.BENCHMARK_DATA = {
             "username": "sya-ri"
           },
           "distinct": true,
-          "id": "100a3fd0352708c65e783ceb8a9d38f94780a726",
-          "message": "ci: Fix benchmark branch",
-          "timestamp": "2024-09-16T23:49:05+09:00",
-          "tree_id": "c4d759a4ba9a7d3ae75d88d4fd55843f3667f326",
-          "url": "https://github.com/sya-ri/ktConfig/commit/100a3fd0352708c65e783ceb8a9d38f94780a726"
-        },
-        "date": 1726498720637,
-        "tool": "jmh",
-        "benches": [
-          {
-            "name": "dev.s7a.ktconfig.Benchmark.single",
-            "value": 0.007161375338282236,
-            "unit": "ms/op",
-            "extra": "iterations: 3\nforks: 1\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "git@s7a.dev",
-            "name": "sya-ri",
-            "username": "sya-ri"
-          },
-          "committer": {
-            "email": "git@s7a.dev",
-            "name": "sya-ri",
-            "username": "sya-ri"
-          },
-          "distinct": true,
-          "id": "662845e04bb8e3c1ec72e6f214321121b06cd9a4",
-          "message": "ci: Fix gh-pages-branch path",
-          "timestamp": "2024-09-17T00:08:15+09:00",
-          "tree_id": "65fca51fc9859fbeb810b8f5bcdb1bf9b3978361",
-          "url": "https://github.com/sya-ri/ktConfig/commit/662845e04bb8e3c1ec72e6f214321121b06cd9a4"
-        },
-        "date": 1726499559063,
-        "tool": "jmh",
-        "benches": [
-          {
-            "name": "dev.s7a.ktconfig.Benchmark.single",
-            "value": 0.007560895509473055,
-            "unit": "ms/op",
-            "extra": "iterations: 3\nforks: 1\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "git@s7a.dev",
-            "name": "sya-ri",
-            "username": "sya-ri"
-          },
-          "committer": {
-            "email": "git@s7a.dev",
-            "name": "sya-ri",
-            "username": "sya-ri"
-          },
-          "distinct": true,
-          "id": "fddda188ccff0b27cc91addbda4551bf242c67b2",
-          "message": "ci: Fix not found benchmark files",
-          "timestamp": "2024-09-17T00:15:58+09:00",
-          "tree_id": "5bfb084127ec8798ecaa6033476a14b939985715",
-          "url": "https://github.com/sya-ri/ktConfig/commit/fddda188ccff0b27cc91addbda4551bf242c67b2"
-        },
-        "date": 1726499954446,
-        "tool": "jmh",
-        "benches": [
-          {
-            "name": "dev.s7a.ktconfig.Benchmark.single",
-            "value": 0.007176845493238178,
-            "unit": "ms/op",
-            "extra": "iterations: 3\nforks: 1\nthreads: 1"
-          }
-        ]
-      },
-      {
-        "commit": {
-          "author": {
-            "email": "git@s7a.dev",
-            "name": "sya-ri",
-            "username": "sya-ri"
-          },
-          "committer": {
-            "email": "git@s7a.dev",
-            "name": "sya-ri",
-            "username": "sya-ri"
-          },
-          "distinct": true,
           "id": "cf71df7375ec7962a260453ce8ad0408d6c6fd16",
           "message": "test: More benchmark tests",
           "timestamp": "2024-09-17T00:37:57+09:00",
@@ -115,6 +25,12 @@ window.BENCHMARK_DATA = {
         "date": 1726501875867,
         "tool": "jmh",
         "benches": [
+          {
+            "name": "dev.s7a.ktconfig.Benchmark.single",
+            "value": 0.007407566238606649,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
           {
             "name": "dev.s7a.ktconfig.Benchmark.list",
             "value": 0.013476180560264324,
@@ -130,6 +46,12 @@ window.BENCHMARK_DATA = {
           {
             "name": "dev.s7a.ktconfig.Benchmark.recursive",
             "value": 0.08873301557621137,
+            "unit": "ms/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.s7a.ktconfig.Benchmark.saveSingle",
+            "value": 0.006095695304688169,
             "unit": "ms/op",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           },
@@ -151,18 +73,6 @@ window.BENCHMARK_DATA = {
             "unit": "ms/op",
             "extra": "iterations: 3\nforks: 1\nthreads: 1"
           },
-          {
-            "name": "dev.s7a.ktconfig.Benchmark.saveSingle",
-            "value": 0.006095695304688169,
-            "unit": "ms/op",
-            "extra": "iterations: 3\nforks: 1\nthreads: 1"
-          },
-          {
-            "name": "dev.s7a.ktconfig.Benchmark.single",
-            "value": 0.007407566238606649,
-            "unit": "ms/op",
-            "extra": "iterations: 3\nforks: 1\nthreads: 1"
-          }
         ]
       },
       {
