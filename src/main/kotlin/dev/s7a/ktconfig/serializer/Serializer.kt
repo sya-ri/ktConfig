882bf61b380ba514d8b1b0c4e74e37d2587a9cc7
sya-ri <git@s7a.dev>
2025-07-14 06:16:58 +0900

    docs: Add comprehensive documentation for serializer classes and methods

--- src/main/kotlin/dev/s7a/ktconfig/serializer/Serializer.kt
+++ src/main/kotlin/dev/s7a/ktconfig/serializer/Serializer.kt
@@ -2,17 +2,49 @@ package dev.s7a.ktconfig.serializer
 
 import org.bukkit.configuration.file.YamlConfiguration
 
+/**
+ * Interface for handling serialization and deserialization of values to/from YAML configuration.
+ * This interface provides methods to read and write values of type [T] in a YAML configuration.
+ *
+ * @param T The type of value to be serialized/deserialized
+ * @since 2.0.0
+ */
 interface Serializer<T> {
+    /**
+     * Gets a value from the configuration at the specified path or throws an exception if not found.
+     *
+     * @param configuration The YAML configuration to read from
+     * @param path The configuration path to read the value from
+     * @return The deserialized value of type [T]
+     * @throws IllegalArgumentException if no value exists at the specified path
+     * @since 2.0.0
+     */
     fun getOrThrow(
         configuration: YamlConfiguration,
         path: String,
     ): T = get(configuration, path) ?: throw IllegalArgumentException("Not found value: $path")
 
+    /**
+     * Gets a value from the configuration at the specified path.
+     *
+     * @param configuration The YAML configuration to read from
+     * @param path The configuration path to read the value from
+     * @return The deserialized value of type [T], or null if the path doesn't exist
+     * @since 2.0.0
+     */
     fun get(
         configuration: YamlConfiguration,
         path: String,
     ): T?
 
+    /**
+     * Saves a value to the configuration at the specified path.
+     *
+     * @param configuration The YAML configuration to write to
+     * @param path The configuration path to save the value at
+     * @param value The value to serialize and save, or null to remove the value
+     * @since 2.0.0
+     */
     fun save(
         configuration: YamlConfiguration,
         path: String,
--- src/main/kotlin/dev/s7a/ktconfig/serializer/TransformSerializer.kt
+++ src/main/kotlin/dev/s7a/ktconfig/serializer/TransformSerializer.kt
@@ -1,5 +1,14 @@
 package dev.s7a.ktconfig.serializer
 
+/**
+ * Abstract serializer that transforms values between two types using a base serializer.
+ * This class allows converting between type T and type B during serialization/deserialization.
+ *
+ * @param T The type to transform to/from
+ * @param B The base type used by the underlying serializer
+ * @property base The base serializer used for the underlying type B
+ * @since 2.0.0
+ */
 abstract class TransformSerializer<T, B>(
     val base: ValueSerializer<B>,
 ) : ValueSerializer<T> {
@@ -7,7 +16,21 @@ abstract class TransformSerializer<T, B>(
 
     override fun serialize(value: T) = base.serialize(transformBack(value))
 
+    /**
+     * Transforms a value from the base type to the target type.
+     *
+     * @param value The value of base type to transform
+     * @return The transformed value of target type
+     * @since 2.0.0
+     */
     abstract fun transform(value: B): T
 
+    /**
+     * Transforms a value from the target type back to the base type.
+     *
+     * @param value The value of target type to transform back
+     * @return The transformed value of base type
+     * @since 2.0.0
+     */
     abstract fun transformBack(value: T): B
 }
