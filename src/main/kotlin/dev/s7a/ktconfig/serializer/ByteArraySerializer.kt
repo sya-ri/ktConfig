b6626f01251c8b79a1478f5e8da821b67d164e1a
sya-ri <git@s7a.dev>
2025-11-19 14:24:04 +0900

    refactor: remove `ByteArraySerializer.Nullable` class
    - Simplify `ByteArraySerializer` by removing the `Nullable` class implementation.
    - Remove redundant nullable handling logic to streamline the serializer.

--- src/main/kotlin/dev/s7a/ktconfig/serializer/ByteArraySerializer.kt
+++ src/main/kotlin/dev/s7a/ktconfig/serializer/ByteArraySerializer.kt
@@ -7,19 +7,6 @@ package dev.s7a.ktconfig.serializer
  * @since 2.0.0
  */
 object ByteArraySerializer : CollectionSerializer<Byte, ByteArray>(ByteSerializer) {
-    /**
-     * Nullable variant of [ByteArraySerializer] that allows null elements.
-     *
-     * @since 2.0.0
-     */
-    class Nullable(
-        valueSerializer: Serializer<Byte>,
-    ) : CollectionSerializer.Nullable<Byte, ByteArray?>(valueSerializer) {
-        override fun toCollection(value: List<Byte?>) = value.filterNotNull().toByteArray()
-
-        override fun toList(value: ByteArray?) = value?.toList() ?: emptyList()
-    }
-
     override fun toCollection(value: List<Byte>) = value.toByteArray()
 
     override fun toList(value: ByteArray) = value.toList()
