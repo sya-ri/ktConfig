f4fae3c470c4b9e1f748eda5838c6d19969f3c8f
sya-ri <git@s7a.dev>
2026-01-04 12:08:46 +0900

    refactor: replace custom publishing logic with Maven Publish plugin
    - Removed custom `applyPublishingConfig` logic and replaced it with the Gradle Maven Publish plugin.
    - Updated `build.gradle.kts` and `ksp/build.gradle.kts` to use `mavenPublishing` configuration.
    - Added Dokka support for Javadoc generation.
    - Updated `libs.versions.toml` to include Maven Publish and Dokka plugin versions.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,7 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import kotlin.jvm.java
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -6,8 +9,9 @@ plugins {
     alias(libs.plugins.kover)
     alias(libs.plugins.kotlinter)
     alias(libs.plugins.pluginYml.bukkit) apply false
-    `maven-publish`
-    signing
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
+    alias(libs.plugins.mavenPublish)
 }
 
 group = "dev.s7a"
@@ -39,15 +43,36 @@ dependencies {
     testImplementation(libs.spigot)
 }
 
-val sourceJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("dev.s7a", "ktConfig", version.toString())
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationJavadoc"),
+            sourcesJar = true,
+        ),
+    )
+    pom {
+        name.set("ktConfig")
+        description.set("Spigot configuration library for Kotlin using class annotations.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/sya-ri/ktConfig")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/sya-ri/ktConfig/blob/master/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("sya-ri")
+                name.set("sya-ri")
+                email.set("contact@s7a.dev")
+            }
+        }
+        scm {
+            url.set("https://github.com/sya-ri/ktConfig")
+        }
+    }
 }
-
-applyPublishingConfig(
-    "ktConfig",
-    publication = {
-        from(components["kotlin"])
-        artifact(sourceJar.get())
-    },
-)
--- ksp/build.gradle.kts
+++ ksp/build.gradle.kts
@@ -1,6 +1,10 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+
 plugins {
-    `maven-publish`
-    signing
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
+    alias(libs.plugins.mavenPublish)
 }
 
 dependencies {
@@ -9,15 +13,38 @@ dependencies {
     implementation(libs.kotlinpoet.ksp)
 }
 
-val sourceJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
+version = rootProject.version
 
-applyPublishingConfig(
-    "ktConfig-ksp",
-    publication = {
-        from(components["kotlin"])
-        artifact(sourceJar.get())
-    },
-)
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("dev.s7a", "ktConfig-ksp", version.toString())
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationJavadoc"),
+            sourcesJar = true,
+        ),
+    )
+    pom {
+        name.set("ktConfig-ksp")
+        description.set("Spigot configuration library for Kotlin using class annotations.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/sya-ri/ktConfig")
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/sya-ri/ktConfig/blob/master/LICENSE")
+            }
+        }
+        developers {
+            developer {
+                id.set("sya-ri")
+                name.set("sya-ri")
+                email.set("contact@s7a.dev")
+            }
+        }
+        scm {
+            url.set("https://github.com/sya-ri/ktConfig")
+        }
+    }
+}
