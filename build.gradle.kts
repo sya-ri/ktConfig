06d689a0fc0d71150b97f9ea8464f3a2c5ad1b6b
sya-ri <git@s7a.dev>
2026-02-11 01:52:10 +0900

    chore: Fix publish config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kover)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.dokka.javadoc)
     alias(libs.plugins.jmh)
     alias(libs.plugins.kotlinter)
     alias(libs.plugins.pluginYml.bukkit) apply false
     alias(libs.plugins.minecraftServer) apply false
     alias(libs.plugins.shadow) apply false
-    `maven-publish`
-    signing
+    alias(libs.plugins.mavenPublish)
 }
 
 group = "dev.s7a"
@@ -64,64 +66,37 @@ jmh {
     resultFormat.set("json")
 }
 
-val sourceJar by tasks.registering(Jar::class) {
-    archiveClassifier.set("sources")
-    from(sourceSets["main"].allSource)
-}
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+    coordinates("dev.s7a", "ktConfig", version.toString())
+    configure(
+        KotlinJvm(
+            javadocJar = JavadocJar.Dokka("dokkaGeneratePublicationJavadoc"),
+            sourcesJar = true,
+        ),
+    )
 
-publishing {
-    repositories {
-        maven {
-            url =
-                uri(
-                    if (version.toString().endsWith("SNAPSHOT")) {
-                        "https://s01.oss.sonatype.org/content/repositories/snapshots/"
-                    } else {
-                        "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
-                    },
-                )
-            credentials {
-                username = properties["sonatypeUsername"].toString()
-                password = properties["sonatypePassword"].toString()
+    pom {
+        name.set("ktConfig")
+        description.set("Spigot config library for Kotlin handled using class constructor")
+        url.set("https://github.com/sya-ri/ktConfig")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://github.com/sya-ri/ktConfig/blob/master/LICENSE")
             }
         }
-    }
-    publications {
-        register<MavenPublication>("maven") {
-            groupId = "dev.s7a"
-            artifactId = "ktConfig"
-            from(components["kotlin"])
-            artifact(sourceJar.get())
-            pom {
-                name.set("ktConfig")
-                description.set("Spigot config library for Kotlin handled using class constructor")
-                url.set("https://github.com/sya-ri/ktConfig")
-
-                licenses {
-                    license {
-                        name.set("MIT License")
-                        url.set("https://github.com/sya-ri/ktConfig/blob/master/LICENSE")
-                    }
-                }
-                developers {
-                    developer {
-                        id.set("sya-ri")
-                        name.set("sya-ri")
-                        email.set("contact@s7a.dev")
-                    }
-                }
-                scm {
-                    url.set("https://github.com/sya-ri/ktConfig")
-                }
+        developers {
+            developer {
+                id.set("sya-ri")
+                name.set("sya-ri")
+                email.set("contact@s7a.dev")
             }
         }
+        scm {
+            url.set("https://github.com/sya-ri/ktConfig")
+        }
     }
-}
-
-signing {
-    val key = properties["signingKey"]?.toString()?.replace("\\n", "\n")
-    val password = properties["signingPassword"]?.toString()
-
-    useInMemoryPgpKeys(key, password)
-    sign(publishing.publications["maven"])
 }
